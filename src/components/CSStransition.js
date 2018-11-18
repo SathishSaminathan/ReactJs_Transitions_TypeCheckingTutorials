00191de5043dc61094305d7643fcf1d12abd6b8e
SathishSaminathan <Sathishcse1975@gmail.com>
2018-11-18 20:16:01 +0530

    commit for the CSSTransition

--- src/components/CSStransition.js
+++ src/components/CSStransition.js
@@ -1,16 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import '../css/App.css';
+import { CSSTransition } from "react-transition-group";
 
-class Fade extends Component{
-    render(){
-        return(
-            <div>
-                CSS TRANSITIONS
-            </div>
-        )
-    }
+import "../css/App.css";
+
+class Fade extends Component {
+  state = {
+    show: false
+  };
+
+  showDiv = () => {
+    this.setState({
+      show: !this.state.show
+    });
+    return this.state.show;
+  };
+  render() {
+    return (
+      <div>
+        <CSSTransition in={this.state.show} timeout={5000} classNames="square">
+          {/** classNames are used to give the name for the classes in CSSTransition,
+          Since we are giving classNames="square", it will generate the classes like "square-enter,square-enter-active,square-exit,square-enter-active"
+        */}
+          <div className="square">helloooo</div>
+        </CSSTransition>
+        <div className="showDiv" onClick={this.showDiv}>
+          {this.state.show ? "Hide" : "Show"}
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default Fade;
